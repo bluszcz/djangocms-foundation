c4f6eb178f27c4be42a02d1a67bb12317e098ea5
Rafal Zawadzki <bluszcz@bluszcz.net>
2016-04-01 23:34:01 +0200

    refactor and travis

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='djangocms-foundation',
     version='0.0.1',
-    packages=['django_sb_admin'],
+    packages=['djangocms_foundation'],
     include_package_data=True,
     license='Apache License version 2.0',
     description='Foundation 6 plugin for Django CMS.',
